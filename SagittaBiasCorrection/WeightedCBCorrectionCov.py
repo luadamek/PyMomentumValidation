9f98c0441dde015dc9e43919b6e9ff29ef306871
Lukas Adamek <ladamek@gra-login1.graham.sharcnet>
2021-05-17 13:50:53 -0400

    Add code to full cov stacco

--- SagittaBiasCorrection/WeightedCBCorrectionCov.py
+++ SagittaBiasCorrection/WeightedCBCorrectionCov.py
@@ -1,86 +1,168 @@
 from variables import recalc_id_mass, recalc_me_mass, calc_recalc_id_mass, calc_recalc_me_mass, calc_recalc_cb_mass, recalc_cb_mass
 import numpy as np
 
+def stat_comb(vec1, mat1, vec2, mat2):
+    # vec1, vec2: shape = (N events, 5, )
+    # mat1, mat2: shape = (N events, 5, 5)
+    inv_mat1 = np.linalg.pinv(mat1)
+    inv_mat2 = np.linalg.pinv(mat2)
+    res_mat = np.linalg.pinv(inv_mat1 + inv_mat2)
+    first_dot = np.einsum("...i,...ij", vec1, inv_mat1)#np.dot(vec1,inv_mat1)
+    second_dot = np.einsum("...i,...ij", vec2, inv_mat2)#np.dot(vec2,inv_mat2)
+    res_vec = np.einsum("...i,...ij", first_dot + second_dot, res_mat)#np.dot(first_dot + second_dot, res_mat)
+    return res_vec, res_mat
 
-def get_calib_pt(data, charge="", uncorr=False):
+def get_calib_pt(data, charge="", uncorr=False, do_full_matrix = False):
+
     id_pt_string = "{}_ID_Pt".format(charge)
     id_eta_string = "{}_ID_Eta".format(charge)
     id_phi_string = "{}_ID_Phi".format(charge)
+    id_charge_string = "{}_ID_Charge".format(charge)
 
     me_pt_string = "{}_ME_Pt".format(charge)
     me_eta_string = "{}_ME_Eta".format(charge)
     me_phi_string = "{}_ME_Phi".format(charge)
+    me_charge_string = "{}_ME_Charge".format(charge)
 
     if uncorr:
         id_pt_string += "_UNCORR"
-
         me_pt_string += "_UNCORR"
 
     id_cov_string = "{}_ID_TrackCovMatrix".format(charge)
     me_cov_string = "{}_ME_TrackCovMatrix".format(charge)
 
-    over_p_id = 1.0/(data[id_pt_string] * np.cosh(data[id_eta_string]))
-    res_id = data[id_cov_string][:,-1] * 1000.0 * 1000.0
+    id_track_pars = "{}_ID_TrackPars".format(charge)
+    me_track_pars = "{}_ME_TrackPars".format(charge)
 
-    over_p_me = 1.0/(data[me_pt_string] * np.cosh(data[me_eta_string]))
-    res_me = data[me_cov_string][:,-1] * 1000.0 * 1000.0
+    if not do_full_matrix:
 
-    w = (res_me ** 2)/( (res_me**2) + (res_id**2) )
+        over_p_id = 1.0/(data[id_pt_string] * np.cosh(data[id_eta_string]))
+        res_id = data[id_cov_string][:,-1, -1] * 1000.0 * 1000.0
 
-    over_p_cb = (w * over_p_id) + ( (1.0 - w) * over_p_me)
+        over_p_me = 1.0/(data[me_pt_string] * np.cosh(data[me_eta_string]))
+        res_me = data[me_cov_string][:,-1, -1] * 1000.0 * 1000.0
 
-    safe = (data[me_pt_string] > 0) & ( data[id_pt_string] > 0)
+        w = (res_me)/( (res_me) + (res_id) )
 
-    pts = np.ones(len(data["Pos_CB_Pt"]))
+        over_p_cb = (w * over_p_id) + ( (1.0 - w) * over_p_me)
 
-    pts[safe] = ((1.0 / over_p_cb[safe]) / np.cosh(data[id_eta_string][safe]))
+        safe = (data[me_pt_string] > 0) & ( data[id_pt_string] > 0)
 
-    # if not safe, take ID pt
-    id_safe = np.logical_not(safe) & (data[id_pt_string] > 0)
-    pts[id_safe] = data[id_pt_string][id_safe]
+        pts = np.ones(len(data["Pos_CB_Pt"]))
 
-    #otherwise take the ME pt
-    id_unsafe = np.logical_not(safe) & (data[me_pt_string] > 0)
-    pts[id_unsafe] = data[me_pt_string][id_unsafe]
+        pts[safe] = ((1.0 / over_p_cb[safe]) / np.cosh(data[id_eta_string][safe]))
 
-    return pts
+        pts[np.logical_not(safe)] = data["{}_CB_Pt".format(charge)][np.logical_not(safe)] #if there is no ID or ME track, default to the CB track
+        return pts, data["{}_CB_Eta".format(charge)], data["{}_CB_Phi".format(charge)]
+
+    else:
+
+        track_pars_id = data[id_track_pars] # N x 5 array (N events x 5 track pars)
+        track_pars_me = data[me_track_pars]
+
+        #put the corrected pt params into the track pars matrix
+        id_qop = data[id_charge_string] / (data[id_pt_string] * np.cosh(data[id_eta_string])  * 1000.0) #convert to 1/MeV
+        me_qop = data[me_charge_string] / (data[me_pt_string] * np.cosh(data[me_eta_string])  * 1000.0)
+
+        print(id_qop)
+        print(track_pars_id[:,-1])
+
+        #insert the corrected variables into the track params vector
+        track_pars_id[:,-1] = id_qop
+        track_pars_me[:,-1] = me_qop
+
+        cov_mat_id = data[id_cov_string] # N x 5 x 5 array (N events x 5 track pars x 5 track pars)
+        cov_mat_me = data[me_cov_string]
+
+        stacco_pars = stat_comb(track_pars_id, cov_mat_id, track_pars_me, cov_mat_me)
+        track_pars = stacco_pars[0]
+
+        qop = track_pars[:, 4]
+        theta = track_pars[:, 3]
+        phi = track_pars[:, 2]
+        z0 = track_pars[:, 1]
+        d0 = track_pars[:, 0]
+
+        data["{}_CB_Charge".format(charge)] = np.sign(qop)
+
+        p = np.abs((1.0/qop))
+        eta = -1.0 * np.log(np.tan(theta/2.0))
+        pt = (p/np.cosh(eta)) / 1000.0 #convert to GeV
+        print(pt)
+        input()
+
+        return pt, eta, phi
+
 
 
 class WeightedCBCorrectionCov:
 
     #the histogram is a segitta bias correction map for delta s
-    def __init__(self, do_percent_corr=False):
+    def __init__(self, do_percent_corr=False, do_full_matrix = False):
         self.branches = self.get_branches()
         self.do_percent_corr=do_percent_corr
+        self.do_full_matrix = do_full_matrix
 
     def get_branches(self):
         branches = []
         for region in ["ID", "ME", "CB"]:
-            for uncalib, matrix in zip(["Pos_{}_Pt", "Neg_{}_Pt"],  ["Pos_{}_TrackCovMatrix", "Neg_{}_TrackCovMatrix"]):
+            for uncalib, matrix in zip(["Pos_{}_Pt", "Neg_{}_Pt", "Pos_{}_TrackPars", "Pos_{}_Charge"],  ["Pos_{}_TrackCovMatrix", "Neg_{}_TrackCovMatrix", "Neg_{}_TrackPars", "Neg_{}_Charge"]):
                 branches.append(uncalib.format(region))
-                if region != "CB": branches.append(matrix.format(region))
+                branches.append(matrix.format(region))
                 branches+=calc_recalc_id_mass.branches
                 branches+=calc_recalc_me_mass.branches
                 branches+=calc_recalc_cb_mass.branches
+                branches = list(set(branches))
         return branches
 
     def calibrate(self, data):
         print("Track Combination")
 
-        pos_pts_corr = get_calib_pt(data, charge="Pos", uncorr=False)
-        neg_pts_corr = get_calib_pt(data, charge="Neg", uncorr=False)
+        pos_pts_corr, pos_etas_corr, pos_phis_corr = get_calib_pt(data, charge="Pos", uncorr=False, do_full_matrix = self.do_full_matrix)
+        neg_pts_corr, neg_etas_corr, neg_phis_corr = get_calib_pt(data, charge="Neg", uncorr=False, do_full_matrix = self.do_full_matrix)
 
         if self.do_percent_corr and "Pos_ID_Pt_UNCORR" in data:
             print("Doing percent change correction")
-            pos_pts_uncorr = get_calib_pt(data, charge="Pos", uncorr=True)
-            neg_pts_uncorr = get_calib_pt(data, charge="Neg", uncorr=True)
+            pos_pts_uncorr, pos_etas_uncorr, pos_phis_uncorr = get_calib_pt(data, charge="Pos", uncorr=True, do_full_matrix = self.do_full_matrix)
+            neg_pts_uncorr, neg_etas_uncorr, neg_phis_uncorr = get_calib_pt(data, charge="Neg", uncorr=True, do_full_matrix = self.do_full_matrix)
 
-            data["Pos_CB_Pt"] = (1.0 + ((pos_pts_corr - pos_pts_uncorr)/pos_pts_uncorr)) * data["Pos_CB_Pt"]
-            data["Neg_CB_Pt"] = (1.0 + ((neg_pts_corr - neg_pts_uncorr)/neg_pts_uncorr)) * data["Neg_CB_Pt"]
+            cbpt_pos = "Pos_CB_Pt"
+            cbpt_neg = "Neg_CB_Pt"
+
+            cbeta_pos = "Pos_CB_Eta"
+            cbeta_neg = "Neg_CB_Eta"
+
+            cbphi_pos = "Pos_CB_Phi"
+            cbphi_neg = "Neg_CB_Phi"
+
+            if "Pos_CB_Pt_UNCORR" in data:
+                cbpt_pos += "_UNCORR"
+                cbpt_neg += "_UNCORR"
+
+                cbeta_pos += "_UNCORR"
+                cbeta_neg += "_UNCORR"
+
+                cbphi_pos += "_UNCORR"
+                cbphi_neg += "_UNCORR"
+
+            data["Pos_CB_Pt"] = (1.0 + ((pos_pts_corr - pos_pts_uncorr)/pos_pts_uncorr)) * data[cbpt_pos]
+            data["Neg_CB_Pt"] = (1.0 + ((neg_pts_corr - neg_pts_uncorr)/neg_pts_uncorr)) * data[cbpt_neg]
+
+            data["Pos_CB_Eta"] = (1.0 + ((pos_etas_corr - pos_etas_uncorr)/pos_etas_uncorr)) * data[cbeta_pos]
+            data["Neg_CB_Eta"] = (1.0 + ((neg_etas_corr - neg_etas_uncorr)/neg_etas_uncorr)) * data[cbeta_neg]
+
+            data["Pos_CB_Phi"] = (1.0 + ((pos_phis_corr - pos_phis_uncorr)/pos_phis_uncorr)) * data[cbphi_pos]
+            data["Neg_CB_Phi"] = (1.0 + ((neg_phis_corr - neg_phis_uncorr)/neg_phis_uncorr)) * data[cbphi_neg]
 
         elif not self.do_percent_corr:
             data["Pos_CB_Pt"] = pos_pts_corr
             data["Neg_CB_Pt"] = neg_pts_corr
+
+            data["Pos_CB_Eta"] = pos_etas_corr
+            data["Neg_CB_Eta"] = neg_etas_corr
+
+            data["Pos_CB_Phi"] = pos_phis_corr
+            data["Neg_CB_Phi"] = neg_phis_corr
 
         if hasattr(data, "dtype"): keys =  data.dtype.names
         else: keys = list(data.keys())
--- Submission/prepare_submission.py
+++ Submission/prepare_submission.py
@@ -324,7 +324,7 @@ def apply_systematic_variations(hist_fillers, stat_syst = False, resbias_syst = 
                     hist_filler.apply_calibration_for_channel(channel + "_{}".format(detector_location), calib, selections=selections)
 
 
-def submit_jobs(tree_name, job_name, n_jobs, queue_flavour, file_flavour, filling_script, slurm_directories, load_calibrations, inject = None, inject_channels="", default_correction=False, cov_combination=False, memory="6000M", latest_mc_correction = False, simple_combination = False, skip_mass_recalc=False, skip_baseline_selection = False, add_BDT_combination=False, cov_combination_percent=False):
+def submit_jobs(tree_name, job_name, n_jobs, queue_flavour, file_flavour, filling_script, slurm_directories, load_calibrations, inject = None, inject_channels="", default_correction=False, cov_combination=False, memory="6000M", latest_mc_correction = False, simple_combination = False, skip_mass_recalc=False, skip_baseline_selection = False, add_BDT_combination=False, cov_combination_percent=False, fullcov_combination_percent=False, fullcov_combination=False):
         from DefaultCalibration import DefaultCorrection
         project_dir = os.getenv("MomentumValidationDir")
         assert project_dir is not None
@@ -486,6 +486,13 @@ def submit_jobs(tree_name, job_name, n_jobs, queue_flavour, file_flavour, fillin
         if cov_combination_percent:
             for hist_filler in filler_list: hist_filler.apply_calibration_for_channel("__ALL__", WeightedCBCorrectionCov(do_percent_corr=True))
 
+        if fullcov_combination_percent:
+            for hist_filler in filler_list: hist_filler.apply_calibration_for_channel("__ALL__", WeightedCBCorrectionCov(do_percent_corr=True, do_full_matrix=True))
+
+        if fullcov_combination:
+            for hist_filler in filler_list: hist_filler.apply_calibration_for_channel("__ALL__", WeightedCBCorrectionCov(do_percent_corr=False, do_full_matrix=True))
+
+
         from DefaultSimpleCombination import DefaultCombination
         if simple_combination:
             for hist_filler in filler_list: hist_filler.apply_calibration_for_channel("__ALL__", DefaultCombination())
@@ -528,6 +535,8 @@ if __name__ == "__main__":
         parser.add_argument('--latest_mc_correction', "-def_mc_calib", dest="latest_mc_correction", action="store_true")
         parser.add_argument('--cov_combination', '-cov_comb', dest="cov_combination", action="store_true")
         parser.add_argument('--cov_combination_percent', '-cov_comb_per', dest="cov_combination_percent", action="store_true")
+        parser.add_argument('--fullcov_combination', '-fullcov_comb', dest="fullcov_combination", action="store_true")
+        parser.add_argument('--fullcov_combination_percent', '-fullcov_comb_per', dest="fullcov_combination_percent", action="store_true")
         parser.add_argument('--simple_combination', '-simple_comb', dest="simple_combination", action="store_true")
         parser.add_argument('--skip_mass_recalc', '-smrecalc', dest="skip_mass_recalc", action="store_true")
         parser.add_argument('--skip_baseline_selection', '-skbase', dest="skip_baseline_selection", action="store_true")
@@ -548,7 +557,7 @@ if __name__ == "__main__":
             inject = None
         slurm_directories = ["/project/def-psavard/ladamek/momentumvalidationoutput/", args.job_name]
 
-        jobset_file, slurm_directory = submit_jobs(tree_name, job_name, n_jobs, queue_flavour, file_flavour, filling_script, slurm_directories, args.load_calibrations, inject=inject, inject_channels=inject_channels, default_correction=args.default_correction, cov_combination=args.cov_combination, memory=args.memory, latest_mc_correction = args.latest_mc_correction, simple_combination=args.simple_combination, skip_mass_recalc=args.skip_mass_recalc, skip_baseline_selection=args.skip_baseline_selection, add_BDT_combination = args.add_BDT_combination, cov_combination_percent=args.cov_combination_percent)
+        jobset_file, slurm_directory = submit_jobs(tree_name, job_name, n_jobs, queue_flavour, file_flavour, filling_script, slurm_directories, args.load_calibrations, inject=inject, inject_channels=inject_channels, default_correction=args.default_correction, cov_combination=args.cov_combination, memory=args.memory, latest_mc_correction = args.latest_mc_correction, simple_combination=args.simple_combination, skip_mass_recalc=args.skip_mass_recalc, skip_baseline_selection=args.skip_baseline_selection, add_BDT_combination = args.add_BDT_combination, cov_combination_percent=args.cov_combination_percent, fullcov_combination_percent=args.fullcov_combination_percent, fullcov_combination=args.fullcov_combination)
 
         print("Job saved in {}, the jobset is {}".format(slurm_directory, jobset_file))
         #submit the jobs, and wait until completion
